22902a1ae9a21d3b7aa9f0b30f1db0c657212b84
Y. Benjamin Perez C. <y.benjamin_pc@hotmail.com>
2025-05-31 18:30:24 +0100

    Add files via upload

---
+++ Supermarket-Management-System/Iteration1/src/javaapplication1/ProductList.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author 2231349
+ */
+
+
+public class ProductList {
+    private List <Product> productList;
+
+    public ProductList() {
+        this.productList = new ArrayList<>();
+        // Add sample products
+        
+        // Add more products as needed
+    }
+
+    // Additional methods if needed
+}
